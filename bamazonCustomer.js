3cdb81829f129a7dca44b3e0fe91c0f6454c2290
Aaron.Gauntt <gauntta@gmail.com>
2019-03-21 17:43:33 -0500

    headache decreasing

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,43 +21,37 @@ var connection = mysql.createConnection({
     if (err) throw err;
     //run the start function after the connection is made to prompt the user
     displayWares();
-    start();
     connection.end();
   });
 
-
   function displayWares() {
-    var query = connection.query("SELECT * FROM Bamazon.products", function(err, res){
+    console.log("--------------- WELCOME TO BAMAZON!! ---------------------")
+    connection.query("SELECT * FROM Bamazon.products", function(err, res){
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log("PRODUCT ID: " + res[i].id + "   PRODUCT NAME: " + res[i].product_name + "   DEPARTMENT: " + res[i].department_name + "   PRICE: " + res[i].price);
-    }
-    
-})
-  
-}
-
-  function start() {
-      // function which prompts the user for what action they should take
-    inquirer
-      .prompt({
-        name: "welcome",
-        type: "input",
-        message: "Welcome to Bamazon! Please inter the product ID of the item you would like to buy"
-      },
-      {
-          name: "quantity",
-          type: "input",
-          message: "Great! Now, select how many units you would like to buy"
-      })
-      .then(function(answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.welcome === "NaN") {
-            console.log("Im sorry, we couldnt find the item you were looking for");
-            return;
-        } else console.log("will it work");
-      });
-  }
+            console.log("----------------------------------------------------------------------")
+        }
+       
+            inquirer.prompt({
+              name: "welcome",
+              type: "input",
+              message: "Welcome to Bamazon! Please inter the product ID of the item you would like to buy"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "Great! Now, select how many units you would like to buy"
+            })
+            .then(function(answer) {
+              // based on their answer, either call the bid or the post functions
+              if (answer.welcome === "NaN") {
+                  console.log("Im sorry, we couldnt find the item you were looking for");
+                  return;
+              } else console.log("will it work");
+          })
+        })
+      }
  
 
 
